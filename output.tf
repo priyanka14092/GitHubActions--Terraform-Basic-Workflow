ac5dd26b76b129a21c63f6aee4034ce3d6ea5477
Priyanka <77484416+priyanka14092@users.noreply.github.com>
2023-02-28 19:06:36 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,10 +1,10 @@
-# Output Values
-# EC2 Instance Names
-output "op-instance"{
-    value = aws_instance.myInstance[*].tags.Name
-}
+# # Output Values
+# # EC2 Instance Names
+# output "op-instance"{
+#     value = aws_instance.myInstance[*].tags.Name
+# }
 
-# S3 Bucket Names
-output "op-bucket"{
-    value = {for k, v in var.bucketNames : k => v}
-}
+# # S3 Bucket Names
+# output "op-bucket"{
+#     value = {for k, v in var.bucketNames : k => v}
+# }
