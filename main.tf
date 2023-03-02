35f8276eb41ce0d266e174dfed16d63947dd2e50
Priyanka <77484416+priyanka14092@users.noreply.github.com>
2023-03-02 23:41:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # EC2 Instances using count
 resource "aws_instance" "myInstance" {
-  #count         = length(var.instanceNames)
+  count         = length(var.instanceNames)
   ami           = "ami-05fa00d4c63e32376"
   instance_type = var.instance-Type
   tags = {
@@ -11,12 +11,12 @@ resource "aws_instance" "myInstance" {
 }
 
 # S3 Buckets using for_each
-# resource "aws_s3_bucket" "myBucket" {
-#   for_each = var.bucketNames
-#   bucket   = "${each.value}"
-#   tags = {
-#     Name    = "${each.value}"
-#     Owner   = "Priyanka"
-#     Purpose = "Assignment"
-#   }
-# }
+resource "aws_s3_bucket" "myBucket" {
+  for_each = var.bucketNames
+  bucket   = "${each.value}"
+  tags = {
+    Name    = "${each.value}"
+    Owner   = "Priyanka"
+    Purpose = "Assignment"
+  }
+}
